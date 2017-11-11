bc2d351419a26998d41f4788f8f19387b40f4eff
Slimdwet <rinville.evans@gmail.com>
2017-11-11 15:46:51 +0100

    Evolution de la structure BDD

---
+++ dumps/User.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : mariadb:3306
+-- Généré le :  sam. 11 nov. 2017 à 09:36
+-- Version du serveur :  10.2.9-MariaDB-10.2.9+maria~jessie
+-- Version de PHP :  7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `audreycuisine.fr`
+--
+
+--
+-- Déchargement des données de la table `User`
+--
+
+INSERT INTO `User` (`id`, `full_name`) VALUES
+(1, 'Audrey Bourdin');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
