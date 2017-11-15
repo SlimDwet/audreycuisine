4d37f32696c419e21a442dc7a71bb95367ffdeab
Slimdwet <rinville.evans@gmail.com>
2017-11-15 23:24:15 +0100

    MOD: structure de la section des derniers articles + FIX refacto appel ajax qui créait une reconstruction systematique du composant a la mise a jour du state

--- front/src/LastPosts/index.js
+++ front/src/LastPosts/index.js
@@ -1,27 +1,55 @@
 import React from 'react';
+import './index.css';
 import SectionTitle from '../Components/SectionTitle/SectionTitle';
 
 class LastPosts extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            lastPosts: null
+        }
+    }
+
+    componentDidMount() {
+        this.getLastPosts();
+    }
 
     getLastPosts() {
         if(this.props.urlLastPosts === undefined) throw new Error("L'URL vers les derniers articles est manquante");
 
         // On récupère les derniers articles
-        fetch(this.props.urlLastPosts)
-        .then(response => {
-            if(response.status === 200) {
-                response.json().then(data => {
-                    this.lastPosts = data.data;
-                })
-            } else throw new Error("Une erreur s'est produite lors du chargement des derniers articles");
-        });
+        if(!this.state.lastPosts) {
+            fetch(this.props.urlLastPosts)
+            .then(response => {
+                if(response.status === 200) {
+                    response.json().then(data => {
+                        this.setState({
+                            lastPosts: data.data
+                        });
+                    })
+                } else throw new Error("Une erreur s'est produite lors du chargement des derniers articles");
+            });
+        }
     }
 
     render() {
-        this.getLastPosts();
+        let allPosts = [];
+        if(this.state.lastPosts) {
+            this.state.lastPosts.forEach(post => {
+                allPosts.push(
+                    <div className="post">
+                        {/*<img src={post.urlPostThumbnail} />*/}
+                    </div>
+                );
+            })
+        }
         return (
-            <div className="lastPostsContainer">
+            <div>
                 <SectionTitle title="Mes derniers articles publiés" />
+                <div className="lastPostsContainer">
+                    {allPosts}
+                </div>
             </div>
         );
     }
