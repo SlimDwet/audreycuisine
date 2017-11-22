b5eeef011838df6061631b263ef425793d893ca2
Slimdwet <rinville.evans@gmail.com>
2017-11-22 22:45:47 +0100

    MOD: generation du rendu des derniers articles + utilisation des fonctions et constantes utilitaires

--- front/src/LastPosts/index.js
+++ front/src/LastPosts/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './index.css';
 import SectionTitle from '../Components/SectionTitle/SectionTitle';
+import constants from '../utils/constants';
+import {getFrenchDate, getRandomKey} from '../utils/functions';
 
 class LastPosts extends React.Component {
 
@@ -32,54 +34,45 @@ class LastPosts extends React.Component {
         }
     }
 
+    /**
+     * Retourne les X premiers mots du texte comme extrait
+     * @param  {string} text [Texte à partir duquel on extrait le résumé]
+     * @return {string}      [Extrait]
+     */
+    getExcerpt(text) {
+        let words = text.trim().split(/\s+/);
+        let excerpt = '';
+        if(words.length > constants.excerptLength) {
+            // On récupère les X premiers mots pour avoir le résumé
+            let reducedWords = words.slice(0, constants.excerptLength);
+            excerpt = reducedWords.join(' ')+'...';
+        } else {
+            excerpt = text;
+        }
+        return excerpt;
+    }
+
     render() {
         let allPosts = [];
         if(this.state.lastPosts) {
-            // this.state.lastPosts.forEach(post => {
-            //     allPosts.push();
-            // })
-            allPosts.push(
-                <div className="post">
-                    <a href="#">
-                        <img src="https://picsum.photos/300/255/?random" />
-                    </a>
-                    <p className="postContainer">
-                        <span className="postCategories">blabla</span>
-                        <a href="#" className="postLink">post link</a>
-                        <p className="postExcerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quidem eaque praesentium, incidunt deleniti voluptates, assumenda, repellendus, sapiente voluptas temporibus in ullam. Sit excepturi dolorem inventore. Culpa obcaecati minus dicta? Eius ullam deleniti ipsam, voluptate libero sint, molestiae dolore earum sapiente iusto consequatur nostrum corporis ipsum, expedita eum, nihil veniam.</p>
-                    </p>
-                </div>,
-                <div className="post">
-                    <a href="#">
-                        <img src="https://picsum.photos/300/255/?random" />
-                    </a>
-                    <p className="postContainer">
-                        <span className="postCategories">blabla</span>
-                        <a href="#" className="postLink">post link</a>
-                        <p className="postExcerpt">lorem50</p>
-                    </p>
-                </div>,
-                <div className="post">
-                    <a href="#">
-                        <img src="https://picsum.photos/300/255/?random" />
-                    </a>
-                    <p className="postContainer">
-                        <span className="postCategories">blabla</span>
-                        <a href="#" className="postLink">post link</a>
-                        <p className="postExcerpt">lorem50</p>
-                    </p>
-                </div>,
-                <div className="post">
-                    <a href="#">
-                        <img src="https://picsum.photos/300/255/?random" />
-                    </a>
-                    <p className="postContainer">
-                        <span className="postCategories">blabla</span>
-                        <a href="#" className="postLink">post link</a>
-                        <p className="postExcerpt">lorem50</p>
-                    </p>
-                </div>
-            );
+            this.state.lastPosts.forEach(post => {
+                // On récupère les catégories
+                let categories = [];
+                post.category.map(elt => categories.push(elt.name));
+                allPosts.push(
+                    <div className="post" key={getRandomKey('lastPosts')}>
+                        <a href="#">
+                            <img src={post.urlPostThumbnail} />
+                        </a>
+                        <div className="postContainer">
+                            <span className="postCategories">{categories.join(' ')}</span>
+                            <a href="#" className="postLink">{post.title}</a>
+                            <p className="postExcerpt">{this.getExcerpt(post.contentNoHtml)}</p>
+                            <p className="postDate">{getFrenchDate(post.updated)}</p>
+                        </div>
+                    </div>
+                );
+            });
         }
         return (
             <div>
