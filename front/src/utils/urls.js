4d8ace62e1cfe4b61d2b4b7a97deb346b9b08062
Slimdwet <rinville.evans@gmail.com>
2017-11-23 22:49:08 +0100

    ADD: url vers la liste des articles

--- front/src/utils/urls.js
+++ front/src/utils/urls.js
@@ -1,5 +1,6 @@
 const urls = {
-    'lastPosts': "http://audreycuisine-dev.fr/app_dev.php/category/last-posts"
+    'lastPosts': "http://audreycuisine-dev.fr/app_dev.php/category/last-posts",
+    'allPosts': '/archives'
 }
 
 export default urls;
