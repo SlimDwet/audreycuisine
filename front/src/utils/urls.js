9bd3cd1abdceeda4718f1d82c5132b266fc7b18f
Slimdwet <rinville.evans@gmail.com>
2017-11-25 21:25:27 +0100

    ADD: url vers les selections

--- front/src/utils/urls.js
+++ front/src/utils/urls.js
@@ -1,6 +1,7 @@
 const urls = {
     'lastPosts': "http://audreycuisine-dev.fr/app_dev.php/category/last-posts",
-    'allPosts': '/archives'
+    'allPosts': '/archives',
+    'inMyKitchen': 'http://audreycuisine-dev.fr/app_dev.php/selections'
 }
 
 export default urls;
