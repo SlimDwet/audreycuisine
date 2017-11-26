5a908807c39eaf49fdc0758b217271b7e6731f9b
Slimdwet <rinville.evans@gmail.com>
2017-11-26 18:45:21 +0100

    ADD: composant affichant les articles dans les tuiles des selections

---
+++ front/src/InMyKitchen/Tile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Tile.css';
+import {getFrenchDate} from '../utils/functions';
+
+const tile = (props) => {
+    return (
+        <div className="tilePost">
+            <img src={props.thumbnail} alt="Illustration article" />
+            <a href="#">{props.title}</a>
+            <p className="date">{getFrenchDate(props.date)}</p>
+        </div>
+    );
+}
+
+export default tile;
