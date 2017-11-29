c1ce05f5263ca4ea99a26e2a5de1c469b1cbe3ea
Evans RINVILLE <evans.rinville@ergelis.com>
2017-11-29 18:04:58 +0100

    ADD: configuration pour le module medias sociaux

--- front/src/utils/constants.js
+++ front/src/utils/constants.js
@@ -1,5 +1,14 @@
 const constants = {
-    "excerptLength": 30
+    "excerptLength": 30,
+    "socialMedias": [
+        {'order': 1, 'css': 'rss', 'link': '/feed'},
+        {'order': 2, 'css': 'instagram', 'link': 'https://instagram.com/audreycuisine/'},
+        // {'order': 3, 'css': 'facebook', 'link': 'https://www.facebook.com/audreycuisine.fr'},
+        // {'order': 4, 'css': 'twitter', 'link': 'https://twitter.com/audreycuisine'},
+        // {'order': 5, 'css': 'googleplus', 'link': 'https://plus.google.com/u/0/117618348036211786224/posts'},
+        // {'order': 6, 'css': 'tumblr', 'link': 'http://audreycuisine.tumblr.com/'},
+        // {'order': 7, 'css': 'pinterest', 'link': 'https://www.pinterest.com/audreycuisine/'}
+    ]
 }
 
 export default constants;
