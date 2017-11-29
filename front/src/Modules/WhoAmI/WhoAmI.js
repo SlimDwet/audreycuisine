adfcb574e2df2bba76e27cf723a29fce174d1841
Slimdwet <rinville.evans@gmail.com>
2017-11-29 22:46:26 +0100

    ADD: methode retournant les parametres des reseaux sociaux pour utiliser les URLs

--- front/src/Modules/WhoAmI/WhoAmI.js
+++ front/src/Modules/WhoAmI/WhoAmI.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import './WhoAmI.css';
 import ModuleTitle from '../../Components/ModuleTitle/ModuleTitle';
+import utils from '../../utils/constants';
+import urls from '../../utils/urls';
+
+/**
+ * Retourne la description du réseau social recherché
+ * @param  {string} name [Nom du réseau social]
+ * @return {object}      [Paramètres du réseau social]
+ */
+const getSocialMediaByName = name => {
+    let socialMedia = utils.socialMedias.filter(elt => {
+        return elt.css === name
+    });
+    return socialMedia[0];
+}
 
 /**
  * Module Qui se cache derrière Ma P'tite cuisine
  */
 const WhoAmI = () => {
+    const facebook = getSocialMediaByName('facebook');
+    const instagram = getSocialMediaByName('instagram');
+    const twitter = getSocialMediaByName('twitter');
+    const pinterest = getSocialMediaByName('pinterest');
     return (
         <div className="whoAmIContainer modules">
             <ModuleTitle title="Qui se cache derrière Ma P'tite cuisine" />
             <br/>
-            <img src="https://www.audreycuisine.fr/wp-content/uploads/2017/10/37532703230_417b127798_z.jpg" alt="Photo Audrey Bourdin" />
+            <img src="https://www.audreycuisine.fr/wp-content/uploads/2017/10/37532703230_417b127798_z.jpg" alt="Audrey Bourdin" />
             <br/><br/>
             <span className="name">Audrey Bourdin</span>
             <em className="hobbies">Auteur, blogueur & influenceur</em><br/>
@@ -20,9 +38,9 @@ const WhoAmI = () => {
             </p><br/>
             <p className="myStory">
                 Ma P’tite Cuisine c’est une grande communauté, vous êtes plus de <strong>12.000 visiteurs uniques chaque jour</strong> sur le blog et pas moins de <strong>
-                48.000 sur les réseaux sociaux</strong>, tels que <a href="#">Facebook</a>, <a href="#">Instagram</a>, <a href="#">Twitter</a> & <a href="#">Pinterest</a>.
+                48.000 sur les réseaux sociaux</strong>, tels que <a href={facebook.link}>Facebook</a>, <a href={instagram.link}>Instagram</a>, <a href={twitter.link}>Twitter</a> & <a href={pinterest.link}>Pinterest</a>.
             </p><br/>
-            <p className="myStory">Plus d'infos sur moi <a href="#">ici</a>.</p>
+            <p className="myStory">Plus d'infos sur moi <a href={urls.aPropos}>ici</a>.</p>
         </div>
     );
 }
