b5fcbc059c5d0473e7b7d59b7add0a3aeef372e7
Slimdwet <rinville.evans@gmail.com>
2017-11-29 22:45:03 +0100

    MIN: suppression des parametres en commentaires

--- front/src/utils/constants.js
+++ front/src/utils/constants.js
@@ -3,11 +3,11 @@ const constants = {
     "socialMedias": [
         {'order': 1, 'css': 'rss', 'link': '/feed'},
         {'order': 2, 'css': 'instagram', 'link': 'https://instagram.com/audreycuisine/'},
-        // {'order': 3, 'css': 'facebook', 'link': 'https://www.facebook.com/audreycuisine.fr'},
-        // {'order': 4, 'css': 'twitter', 'link': 'https://twitter.com/audreycuisine'},
-        // {'order': 5, 'css': 'googleplus', 'link': 'https://plus.google.com/u/0/117618348036211786224/posts'},
-        // {'order': 6, 'css': 'tumblr', 'link': 'http://audreycuisine.tumblr.com/'},
-        // {'order': 7, 'css': 'pinterest', 'link': 'https://www.pinterest.com/audreycuisine/'}
+        {'order': 3, 'css': 'facebook', 'link': 'https://www.facebook.com/audreycuisine.fr'},
+        {'order': 4, 'css': 'twitter', 'link': 'https://twitter.com/audreycuisine'},
+        {'order': 5, 'css': 'googleplus', 'link': 'https://plus.google.com/u/0/117618348036211786224/posts'},
+        {'order': 6, 'css': 'tumblr', 'link': 'http://audreycuisine.tumblr.com/'},
+        {'order': 7, 'css': 'pinterest', 'link': 'https://www.pinterest.com/audreycuisine/'}
     ]
 }
 
