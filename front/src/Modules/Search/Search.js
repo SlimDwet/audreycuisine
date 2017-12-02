d519ce34bddbc38d0ef35eb747698e39d042e029
Slimdwet <rinville.evans@gmail.com>
2017-12-02 19:38:38 +0100

    MOD: refacto des composants + modifs CSS

--- front/src/Modules/Search/Search.js
+++ front/src/Modules/Search/Search.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import './Search.css';
 import ModuleTitle from '../../Components/ModuleTitle/ModuleTitle';
-import InputSearch from './InputSearch';
-import ButtonSearch from './ButtonSearch';
+import InputSearch from '../../Components/InputSearch/InputSearch';
+import ButtonSearch from '../../Components/ButtonSearch/ButtonSearch';
 
 class Search extends Component {
 
@@ -18,7 +18,8 @@ class Search extends Component {
     inputSearchHandler = evt => {
         evt.preventDefault();
         // Récupération du texte saisi
-        let searchText = evt.target.elements['inputSearch'].value;
+        const inputIndex = 0;
+        let searchText = evt.target.elements[inputIndex].value;
         // Mise à jour du state
         this.setState({
             'searchText': searchText
@@ -29,8 +30,8 @@ class Search extends Component {
         return (
             <form className="searchModuleContainer modules" onSubmit={this.inputSearchHandler}>
                 <ModuleTitle title="Recherche" />
-                <InputSearch searchText={this.state.searchText} />
-                <ButtonSearch />
+                <InputSearch inputPlaceholder="Tapez votre recherche puis Ent" />
+                <ButtonSearch buttonText="" />
             </form>
         );
     }
