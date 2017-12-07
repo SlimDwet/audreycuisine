81a5a23ee9f0572ca0d7f3e581973a26b0fb5d26
Slimdwet <rinville.evans@gmail.com>
2017-12-07 23:28:14 +0100

    ADD: définition des livres

--- front/src/utils/constants.js
+++ front/src/utils/constants.js
@@ -8,6 +8,28 @@ const constants = {
         {'order': 5, 'css': 'googleplus', 'link': 'https://plus.google.com/u/0/117618348036211786224/posts'},
         {'order': 6, 'css': 'tumblr', 'link': 'http://audreycuisine.tumblr.com/'},
         {'order': 7, 'css': 'pinterest', 'link': 'https://www.pinterest.com/audreycuisine/'}
+    ],
+    "books": [
+        {
+            'srcLink': '//ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=FR&ASIN=2759033295&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=maptitecuis-21',
+            'altText': 'Le meilleur de ma p\'tite cuisine'
+        },
+        {
+            'srcLink': 'https://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=2759016722&Format=_SL250_&ID=AsinImage&MarketPlace=FR&ServiceVersion=20070822&WS=1&tag=maptitecuis-21',
+            'altText': '30 jours pour devenir un cordon-bleu'
+        },
+        {
+            'srcLink': 'https://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=2759017001&Format=_SL250_&ID=AsinImage&MarketPlace=FR&ServiceVersion=20070822&WS=1&tag=maptitecuis-21',
+            'altText': 'La cuisine étudiante Nouveau guide'
+        },
+        {
+            'srcLink': 'https://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=2759022714&Format=_SL250_&ID=AsinImage&MarketPlace=FR&ServiceVersion=20070822&WS=1&tag=maptitecuis-21',
+            'altText': '200 recettes pour bébés gourmands'
+        },
+        {
+            'srcLink': 'https://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=2759026183&Format=_SL250_&ID=AsinImage&MarketPlace=FR&ServiceVersion=20070822&WS=1&tag=maptitecuis-21',
+            'altText': '200 recettes pour enfants gourmands'
+        }
     ]
 }
 
