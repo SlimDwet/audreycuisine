4eb18456f07d5316800af5ab5d3725b3f0e9cb94
Slimdwet <rinville.evans@gmail.com>
2017-12-07 23:28:58 +0100

    ADD: composant Vous aimez le thé

---
+++ front/src/Modules/LikeTea/LikeTea.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './LikeTea.css';
+import ModuleTitle from '../../Components/ModuleTitle/ModuleTitle';
+
+/**
+ * Module Vous aimez le thé
+ * @param  {[type]} props [description]
+ * @return {[type]}       [description]
+ */
+const likeTea = props => {
+    return (
+        <div className="modules likeTea">
+            <ModuleTitle title="Vous aimez le thé ?" /><br/>
+            <img src="https://s3-eu-west-1.amazonaws.com/aeup/uploads/programs/544e2aec23b94448048b4944/elements/5a006a2de8facec91d8b4574.gif"
+                alt="Illustration Vous aimez le thé ?" />
+        </div>
+    );
+}
+
+export default likeTea;
