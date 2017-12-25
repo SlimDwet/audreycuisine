1ad8ee69bbc2b5077973eeb190497ff7998ad05d
Slimdwet <rinville.evans@gmail.com>
2017-12-25 15:59:37 +0100

    MOD: le composant Home devient stateless

--- front/src/Home/Home.js
+++ front/src/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Home.css';
 import urls from '../utils/urls';
 import constants from '../utils/constants';
@@ -21,53 +21,50 @@ import ImOnInstagram from '../components/Modules/ImOnInstagram/ImOnInstagram';
 import SocialMediaItemCollection from '../components/Modules/SocialMedias/MediaItems/SocialMediaItemCollection';
 import Carousel from '../components/Carousel/Carousel';
 
-class Home extends Component {
-
-    render() {
-        return (
-            <div className="home">
-                <Header />
-                <div className="container">
-                    <div className="carouselWrapper">
-                        <div className="carouselContainer">
-                            <Carousel slides={constants.carousel} />
-                        </div>
-                        <div className="ad">
-                            <a href="https://www.formulebeaute.com/sabonner/#ae45-1">
-                                <img src="https://lb.affilae.com/imp/598c56825ad25b03a0cc6d08/59eda807bc9c3743ee5c2008/59957237e8facefe0c8b456a/https://s3-eu-west-1.amazonaws.com/aeup/uploads/programs/598c56825ad25b03a0cc6d08/elements/59957237e8facefe0c8b4567.jpeg" />
-                            </a>
-                        </div>
-                    </div><br/>
-                    <div className="leftContent">
-                        <LastPosts urlLastPosts={urls.lastPosts} /><br/><br/>
-                        <ButtonMagenta text="Tous mes articles" link={urls.allPosts} />
-                        <InMyKitchen urlInMyKitchen={urls.inMyKitchen} /><br/>
-                        <SectionTitle title="Suivez-moi sur les réseaux sociaux" /><br/>
-                        <div className="socialNetworkContainer">
-                            <ImOnFacebook />
-                            <ImOnInstagram />
-                        </div>
-                        <p className="printPost">
-                            Pour imprimer mes articles ou les faire découvrir sur vos réseaux sociaux c'est facile : cliquez !
-                        </p>
-                        <ul className="printSocialMedias">
-                            <SocialMediaItemCollection socialMediaList={constants.printSocialMedias} />
-                        </ul>
+const home = props => {
+    return (
+        <div className="home">
+            <Header />
+            <div className="container">
+                <div className="carouselWrapper">
+                    <div className="carouselContainer">
+                        <Carousel slides={constants.carousel} />
                     </div>
-                    <div className="rightContent">
-                        <WhoAmI /><br/>
-                        <SocialMedias /><br/>
-                        <Search /><br/>
-                        <Newsletter /><br/>
-                        <FindRecipe /><br/>
-                        <MyBooks books={constants.books} /><br/>
-                        <LikeTea /><br/>
+                    <div className="ad">
+                        <a href="https://www.formulebeaute.com/sabonner/#ae45-1">
+                            <img src="https://lb.affilae.com/imp/598c56825ad25b03a0cc6d08/59eda807bc9c3743ee5c2008/59957237e8facefe0c8b456a/https://s3-eu-west-1.amazonaws.com/aeup/uploads/programs/598c56825ad25b03a0cc6d08/elements/59957237e8facefe0c8b4567.jpeg" />
+                        </a>
                     </div>
                 </div><br/>
-                <Footer /><br/>
-            </div>
-        );
-    }
+                <div className="leftContent">
+                    <LastPosts urlLastPosts={urls.lastPosts} /><br/><br/>
+                    <ButtonMagenta text="Tous mes articles" link={urls.allPosts} />
+                    <InMyKitchen urlInMyKitchen={urls.inMyKitchen} /><br/>
+                    <SectionTitle title="Suivez-moi sur les réseaux sociaux" /><br/>
+                    <div className="socialNetworkContainer">
+                        <ImOnFacebook />
+                        <ImOnInstagram />
+                    </div>
+                    <p className="printPost">
+                        Pour imprimer mes articles ou les faire découvrir sur vos réseaux sociaux c'est facile : cliquez !
+                    </p>
+                    <ul className="printSocialMedias">
+                        <SocialMediaItemCollection socialMediaList={constants.printSocialMedias} />
+                    </ul>
+                </div>
+                <div className="rightContent">
+                    <WhoAmI /><br/>
+                    <SocialMedias /><br/>
+                    <Search /><br/>
+                    <Newsletter /><br/>
+                    <FindRecipe /><br/>
+                    <MyBooks books={constants.books} /><br/>
+                    <LikeTea /><br/>
+                </div>
+            </div><br/>
+            <Footer /><br/>
+        </div>
+    );
 }
 
-export default Home;
+export default home;
